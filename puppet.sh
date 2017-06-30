cc80163c1441d724dab65829c8841e8c8d8c93cc
Artem Buria <artem_buria@epam.com>
2017-06-30 14:02:33 +0300

    update user-data

--- puppet.sh
+++ puppet.sh
@@ -5,14 +5,16 @@ rpm -Uvh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 yum update -y
 yum -y install puppetserver
 echo "autosign = true" >> /etc/puppetlabs/puppet/puppet.conf
-sed -i '9s/.*/JAVA_ARGS="-Xms512m -Xmx512m"/' "/etc/sysconfig/puppetserver"
+sed -i -e 's/JAVA_ARGS="-Xms2g -Xmx2g"/JAVA_ARGS="-Xms512m -Xmx512m"/' "/etc/sysconfig/puppetserver"
 systemctl start puppetserver
 systemctl enable puppetserver
-cat >> /etc/hosts << EOF "172.16.34.4   slave1
-      172.16.34.5   slave2"
-      EOF
+cat >>/etc/hosts << EOF
+172.16.34.4   slave1
+172.16.34.5   slave2
+EOF
 
-cat >/etc/puppetlabs/code/environments/production/manifests/site.pp << EOF "package { 'ntp' :
+cat >/etc/puppetlabs/code/environments/production/manifests/site.pp << EOF
+package { 'ntp' :
         ensure => installed,
         }
 case $::osfamily{
@@ -26,4 +28,5 @@ case $::osfamily{
           ensure => running,
         }
       }
-    }"EOF
+    }
+EOF
