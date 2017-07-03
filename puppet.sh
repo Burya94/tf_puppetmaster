b68a58336ed95e6eea1a50d37825a7340f6d6135
Artem <buryaslip@yandex.ru>
2017-07-03 10:43:12 +0300

    Update puppet.sh

--- puppet.sh
+++ puppet.sh
@@ -4,7 +4,7 @@ rpm -Uvh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 yum update -y
 yum -y install puppetserver
 echo "autosign = true" >> /etc/puppetlabs/puppet/puppet.conf
-sed -i -e 's/JAVA_ARGS="-Xms2g -Xmx2g"/JAVA_ARGS="-Xms512m -Xmx512m"/' "/etc/sysconfig/puppetserver"
+sed -i -e 's/JAVA_ARGS="-Xms2g -Xmx2g -XX:MaxPermSize=256m"/JAVA_ARGS="-Xms512m -Xmx512m"/' "/etc/sysconfig/puppetserver"
 systemctl start puppetserver
 systemctl enable puppetserver
 cat >/etc/puppetlabs/code/environments/production/manifests/site.pp << EOF
