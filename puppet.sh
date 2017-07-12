41b29cc5d72b5e40c6361564a61f54a2dfbb7f53
Artem Buria <artem_buria@epam.com>
2017-07-12 14:35:02 +0300

    server eyaml install added

--- puppet.sh
+++ puppet.sh
@@ -26,9 +26,9 @@ chown -R puppet:puppet /etc/puppetlabs/puppet/eyaml
 chmod -R 0500 /etc/puppetlabs/puppet/eyaml
 chmod 0400 /etc/puppetlabs/puppet/eyaml/*.pem
 cd /etc/puppetlabs/code/environments/dev/ && /opt/puppetlabs/puppet/bin/r10k puppetfile install
+/opt/puppetlabs/bin/puppetserver gem install hiera-eyaml
 free && sync && echo 3 > /proc/sys/vm/drop_caches && free
 sed -i -e 's/JAVA_ARGS="-Xms2g -Xmx2g -XX:MaxPermSize=256m"/JAVA_ARGS="-Xms512m -Xmx512m"/' "/etc/sysconfig/puppetserver"
-systemctl start puppet
 systemctl start puppetserver
 systemctl enable puppetserver
 #puppetserver gem install hiera-eyaml - need to be run from cli then restart puppetserver
