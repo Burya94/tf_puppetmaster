8dd46413524c38b5a796755f89780a164d041ef0
Artem Buria <artem_buria@epam.com>
2017-07-12 10:42:20 +0300

    script change

--- puppet.sh
+++ puppet.sh
@@ -4,7 +4,6 @@ rpm -Uvh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 yum -y install git
 yum -y install puppetserver
 echo "autosign = true" >> /etc/puppetlabs/puppet/puppet.conf
-sed -i -e 's/JAVA_ARGS="-Xms2g -Xmx2g -XX:MaxPermSize=256m"/JAVA_ARGS="-Xms512m -Xmx512m"/' "/etc/sysconfig/puppetserver"
 /opt/puppetlabs/puppet/bin/gem install r10k
 mkdir -p /etc/puppetlabs/r10k
 
@@ -28,6 +27,7 @@ chmod -R 0500 /etc/puppetlabs/puppet/eyaml
 chmod 0400 /etc/puppetlabs/puppet/eyaml/*.pem
 cd /etc/puppetlabs/code/environments/dev/ && /opt/puppetlabs/puppet/bin/r10k puppetfile install
 free && sync && echo 3 > /proc/sys/vm/drop_caches && free
+sed -i -e 's/JAVA_ARGS="-Xms2g -Xmx2g -XX:MaxPermSize=256m"/JAVA_ARGS="-Xms512m -Xmx512m"/' "/etc/sysconfig/puppetserver"
 systemctl start puppet
 systemctl start puppetserver
 systemctl enable puppetserver
