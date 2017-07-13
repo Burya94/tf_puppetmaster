2c8b0568e3ee0a3d0f0e41fda96d5c0437da3145
Artem Buria <artem_buria@epam.com>
2017-07-13 16:31:14 +0300

    ..

--- puppet.sh
+++ puppet.sh
@@ -20,24 +20,23 @@ EOF
 /opt/puppetlabs/puppet/bin/gem install hiera-eyaml
 mv -f /etc/puppetlabs/code/environments/dev/hiera.yaml /etc/puppetlabs/puppet/hiera.yaml
 sed -i -e 's/JAVA_ARGS="-Xms2g -Xmx2g -XX:MaxPermSize=256m"/JAVA_ARGS="-Xms512m -Xmx512m"/' "/etc/sysconfig/puppetserver"
-----------------------------------
-systemctl stop puppet
-systemctl stop puppetserver
+#----------------------------------
 systemctl start puppet
 free && sync && echo 3 > /proc/sys/vm/drop_caches && free
 systemctl start puppetserver
-systemctl enable puppetserve
------------------------------------
+systemctl enable puppetserver
+systemctl stop puppet
+#-----------------------------------
 mkdir -p /etc/puppetlabs/puppet/eyaml
 /opt/puppetlabs/puppet/bin/eyaml createkeys --pkcs7-private-key=/etc/puppetlabs/puppet/eyaml/private_key.pkcs7.pem --pkcs7-public-key=/etc/puppetlabs/puppet/eyaml/public_key.pkcs7.pem
 chown -R puppet:puppet /etc/puppetlabs/puppet/eyaml
 chmod -R 0500 /etc/puppetlabs/puppet/eyaml
 chmod 0400 /etc/puppetlabs/puppet/eyaml/*.pem
 cd /etc/puppetlabs/code/environments/dev/ && /opt/puppetlabs/puppet/bin/r10k puppetfile install
-------------------------------------------------------
+#------------------------------------------------------
 (crontab -l 2>/dev/null; echo "*/10 * * * * /opt/puppetlabs/puppet/bin/r10k deploy environment") | crontab -
 systemctl restart crond
-#puppetserver gem install hiera-eyaml - need to be run from cli then restart puppetserver
+#Also, the memory requirements will vary based on how many Puppet modules you have in your module path, how much Hiera data you have, etc.
 #/opt/puppetlabs/puppet/bin/eyaml encrypt
 #  --pkcs7-private-key=/etc/puppetlabs/puppet/eyaml/private_key.pkcs7.pem \
 #  --pkcs7-public-key=/etc/puppetlabs/puppet/eyaml/public_key.pkcs7.pem \
