7a4fc2e72ea171e3941c7cf1f1f10fa2a99075d2
Artem Buria <artem_buria@epam.com>
2017-07-17 12:05:34 +0300

    fix

--- variables.tf
+++ variables.tf
@@ -2,4 +2,4 @@ variable "region" {}
 variable "key_name" {}
 variable "instype" {}
 variable "path_to_file" { default = "./puppet.sh"}
-variable "subnet_id" {}
+variable "subnet_id" { type = "list" }
